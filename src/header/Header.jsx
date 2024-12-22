ba81cbbd2e7f7a94ade52974b19b6e2988b26ee4
Madhur-Jatiya <madhurmoms@gmail.com>
2024-12-22 15:49:21 +0530

    new header

--- src/header/Header.jsx
+++ src/header/Header.jsx
@@ -1,48 +1,44 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBars } from "@fortawesome/free-solid-svg-icons";
 import "./Header.css";
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 import profile_img from "../../public/assets/profile.png";
+import { Dropdown, DropdownButton } from "react-bootstrap";
 
 const Header = () => {
-  const [menuOpen, setMenuOpen] = useState(false);
+  const [selectedLanguage, setSelectedLanguage] = useState('LANGUAGE');
 
-  const toggleMenu = () => {
-    setMenuOpen(!menuOpen);
+  const changeLanguage = (language) => {
+    setSelectedLanguage(language);
   };
 
   return (
-    <div className="navbar_container">
-      <nav className="navbar">
-        <div className="navbar-brand">
-          <Link to="/">
-            <img className="brand-logo" src={profile_img} alt=" Logo" />
-          </Link>
-          <Link className="profile_name">MADHUR JATIYA</Link>
-        </div>
-
-        <div className={`navbar-links ${menuOpen ? "active" : ""}`}>
-          <Link className="nav-link" to="#why-choose-us">
-            HOME
-          </Link>
-          <Link className="nav-link" to="#gallery">
-            ABOUT
-          </Link>
-          <Link className="nav-link" to="#about">
-            PROJECTS
-          </Link>
-          <Link className="nav-link" to="#testimonials">
-            CONTACT
-          </Link>
-        </div>
-
-        {/* Menu Toggle Button */}
-        <div className="menu-toggle" onClick={toggleMenu}>
-          <FontAwesomeIcon icon={faBars} />
-        </div>
-      </nav>
-    </div>
+    <Navbar expand="lg" className="navbar_container">
+      <Navbar.Brand href="/" className="profile_name">
+        <img
+          alt=""
+          src={profile_img}
+          width="45"
+          height="45"
+        />
+        <span>MADHUR JATIYA</span>
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="ms-auto navbar_items">
+          <Nav.Link href="/">HOME</Nav.Link>
+          <Nav.Link href="/about">ABOUT</Nav.Link>
+          <Nav.Link href="/project">PROJECTS</Nav.Link>
+          <Nav.Link href="/contact">CONTACT</Nav.Link>
+          <DropdownButton title={selectedLanguage}>
+            <Dropdown.Item onClick={() => changeLanguage('English')}>English</Dropdown.Item>
+            <Dropdown.Item onClick={() => changeLanguage('Hindi')}>Hindi</Dropdown.Item>
+            <Dropdown.Item onClick={() => changeLanguage('German')}>German</Dropdown.Item>
+            <Dropdown.Item onClick={() => changeLanguage('French')}>French</Dropdown.Item>
+          </DropdownButton>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
   );
 };
 
