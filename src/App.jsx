4c48523d38c0daba2092484229d807262183267f
Madhuri-Giri <madhurigoswami23475@gmail.com>
2024-12-28 13:31:47 +0530

    responsive  footer for all devices

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,21 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Header from './header/Header';
-import React from 'react';
-import Home from './home/Home';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./header/Header";
+import React from "react";
+import Home from "./home/Home";
+import Footer from "./footer/Footer";
 
 function App() {
-
-  return <>
-    <Router>
-      <Header />
-      <Routes>
-        <Route path='/' element={<Home />} />
-      </Routes>
-    </Router>
-  </>;
+  return (
+    <>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </>
+  );
 }
 
 export default App;
---
+++ src/footer/Footer.jsx
@@ -0,0 +1,86 @@
+import React from "react";
+import { Row, Col, Container } from "react-bootstrap";
+import "./Footer.css";
+import { TiSocialLinkedin, TiSocialTwitter } from "react-icons/ti";
+import { FaGithub, FaInstagram, FaFacebookSquare, FaHackerrank } from "react-icons/fa";
+import { SiGeeksforgeeks, SiLeetcode } from "react-icons/si";
+
+const Footer = () => {
+  return (
+    <div className="footer_main">
+      <Container>
+        <Row>
+
+          <Col lg={6}>
+            <div className="footer_container">
+              <h1>Madhur Jatiya</h1>
+              <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quis
+                natus, ipsam suscipit nihil aperiam cum culpa? Assumenda,
+                officia. Officia id temporibus vitae tenetur sed sit expedita,
+                mollitia excepturi illum quasi.
+              </p>
+            </div>
+          </Col>
+
+          <Col lg={6}>
+            <div className="footer_box">
+              <h1>Social</h1>
+            </div>
+
+            <div className="social_icon">
+
+              <div className="social_icon_box">
+                <FaGithub />
+              </div>
+
+              <div className="social_icon_con">
+                <TiSocialLinkedin />
+              </div>
+
+              <div className="social_icon_box_cons">
+                <TiSocialTwitter />
+              </div>
+
+              <div className="social_icon_box">
+                <FaInstagram />
+              </div>
+
+              <div className="social_icon_box">
+                <FaFacebookSquare />
+              </div>
+
+            </div>
+
+            <div className="coding">
+
+              <div className="coding_platform">
+                <FaHackerrank />
+              </div>
+
+              <div className="coding_platform">
+                <SiLeetcode />
+              </div>
+
+              <div className="coding_platform">
+                <SiGeeksforgeeks />
+              </div>
+
+            </div>
+          </Col>
+
+        </Row>
+        <hr />
+
+        <div className="copy_right">
+          <p>
+            Copyright 2024. Made by <a href="/">Madhur Jatiya</a>
+          </p>
+        </div>
+        
+      </Container>
+    </div>
+  );
+};
+
+export default Footer;
