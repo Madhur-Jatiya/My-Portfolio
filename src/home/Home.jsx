9892a9810ad1e40e9cbb587c4ba3781f81f37a95
Madhuri-Giri <madhurigoswami23475@gmail.com>
2024-12-29 09:47:59 +0530

    Implement Home page

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -1,15 +1,25 @@
-import React from 'react'
-import './Home.css'
+import React from "react";
+import "./Home.css";
+import { Container } from "react-bootstrap";
 
 const Home = () => {
   return (
-    <>
-      <div className='home_container'>
-        <h1>My Portfolio</h1>
-      </div>
-    </>
+    <div className="home_container">
+      <Container>
+        <div className="home_box">
+          <h1>Hey, I'm Madhur Jatiya</h1>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Beatae,
+            fugit vel accusantium libero quidem blanditiis deleniti quam? Dolor
+            nisi voluptatem dicta officiis, quaerat nihil architecto?
+          </p>
+        </div>
+        <div className="home_button">
+          <button>project</button>
+        </div>
+      </Container>
+    </div>
+  );
+};
 
-  )
-}
-
-export default Home
+export default Home;
