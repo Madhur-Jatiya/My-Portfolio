331bf11231a43981a89ce910155503c79389dd0b
Madhuri-Giri <madhurigoswami23475@gmail.com>
2024-12-29 10:16:27 +0530

    implement contact page

---
+++ src/contact/Contact.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Container, Form, Button } from "react-bootstrap";
+import "./contact.css";
+
+const Contact = () => {
+  return (
+    <div className="contact_container">
+      <Container>
+        <div className="contact_heading">
+          <h1>Contact</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique,
+            ut.
+          </p>
+        </div>
+        <div className="contact_form"></div>
+      </Container>
+    </div>
+  );
+};
+
+export default Contact;
