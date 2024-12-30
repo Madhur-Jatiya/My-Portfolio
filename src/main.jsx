ce5778d598e2640de59d184dfd6e4e6c091f5e1f
unknown <madhurigoswami23475@gmail.com>
2024-12-30 14:32:03 +0530

    correction in src folder

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,13 @@
-/* eslint-disable no-unused-vars */
+import { StrictMode } from 'react'
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
-import { Provider } from 'react-redux';
-import store from './redux/store';
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-<Provider store={store}>
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
     <App />
-</Provider>
-);
-
+  </StrictMode>,
+)
--- src/redux/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-// import loginSlice from '../redux/slice/loginSlice';
-
-const store = configureStore({
-  reducer: {
-    //  loginReducer: loginSlice,
-  }
-})
-export default store
